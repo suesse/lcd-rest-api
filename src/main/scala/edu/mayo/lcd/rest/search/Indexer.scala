9cea9e568ef1f821a153d2b23c1c7b679956b5be
suesse <dale.suesse@live.com>
2013-07-17 13:51:55 -0500

    Changes the way definitions are indexed to reduce redundant fields

--- src/main/scala/edu/mayo/lcd/rest/search/Indexer.scala
+++ src/main/scala/edu/mayo/lcd/rest/search/Indexer.scala
@@ -11,85 +11,89 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.search.{TopScoreDocCollector, IndexSearcher}
 import org.apache.lucene.queryparser.classic.QueryParser
 import java.io.File
-import edu.mayo.lcd.rest.model.DefinitionKeywordsTable
+import edu.mayo.lcd.rest.model._
 
 class Indexer {
 
-  object Definition extends Table[(String, String, String, String)]("definition") {
-    def id = column[String]("id", O.PrimaryKey)
-    def keyword = column[String]("keyword", O.NotNull)
-    def property = column[String]("property", O.NotNull)
-    def value = column[String]("value")
-    def * = id ~ keyword ~ property ~ value
-  }
-
-  object DefinitionProperties extends Table[(String, String, String)]("definition_properties") {
-    def definitionId = column[String]("definitionId", O.NotNull)
-    def property = column[String]("property", O.NotNull)
-    def value = column[String]("value")
-    def * = definitionId ~ property ~ value
-  }
-
-  def selectDefs = {
+  def indexDefs = {
     println("Definitions:")
+    val analyzer = new StandardAnalyzer(Version.LUCENE_43)
+    val iwc = new IndexWriterConfig(Version.LUCENE_43, analyzer)
+    val writer = new IndexWriter(index, iwc)
     Database.forURL("jdbc:mysql://localhost:3306/lcd_rest", user = "root", password = "root", driver = "com.mysql.jdbc.Driver") withSession {
-      Query(Definition) foreach { case (id, keyword, property, value) =>
-        println(" " + id + "\t" + keyword + "\t" + property + "\t" + value)
+      Query(Definition) foreach { case (id, uploader, date) =>
+        val doc = new Document
+        doc.add(new StringField("id", id, Field.Store.YES))
+
+        val keywordQuery = for {
+          k <- Keyword
+          kwv <- DefinitionKeywords if kwv.definitionId eq id
+        } yield k
+        keywordQuery.foreach(k => doc.add(new StringField("keyword", k._2, Field.Store.YES)))
+        val propertyQuery = for {
+          dp <- DefinitionProperty if dp.definitionId eq id
+          pv <- PropertyValue if pv.id eq dp.propertyValueId
+          p <- Property if p.id eq pv.propertyId
+        } yield (p.property, pv.value)
+        propertyQuery.foreach(p => doc.add(new StringField(p._1, p._2, Field.Store.YES)))
+
+        writer.addDocument(doc)
       }
+      writer.close()
     }
   }
 
   def selectDefKeywords = {
     println("Keywords:")
     Database.forURL("jdbc:mysql://localhost:3306/lcd_rest", user = "root", password = "root", driver = "com.mysql.jdbc.Driver") withSession {
-      Query(DefinitionKeywordsTable) foreach { case (definitionId, keyword) =>
+      Query(DefinitionKeywords) foreach { case (definitionId, keyword) =>
         println(" " + definitionId + "\t" + keyword)
       }
     }
   }
 
-  def selectDefProperties = {
-    println("Properties:")
-    Database.forURL("jdbc:mysql://localhost:3306/lcd_rest", user = "root", password = "root", driver = "com.mysql.jdbc.Driver") withSession {
-      Query(DefinitionProperties) foreach { case (definitionId, property, value) =>
-        println(" " + definitionId + "\t" + property + "\t" + property)
-      }
-    }
-  }
+//  def selectDefProperties = {
+//    println("Properties:")
+//    Database.forURL("jdbc:mysql://localhost:3306/lcd_rest", user = "root", password = "root", driver = "com.mysql.jdbc.Driver") withSession {
+//      Query(DefinitionProperties) foreach { case (definitionId, property, value) =>
+//        println(" " + definitionId + "\t" + property + "\t" + property)
+//      }
+//    }
+//  }
 
-//  val index = new SimpleFSDirectory(new File("/Users/m091355/Downloads/lucene"))
-  val index = new RAMDirectory()
-  def indexAll {
-    val analyzer = new StandardAnalyzer(Version.LUCENE_43)
-    Database.forURL("jdbc:mysql://localhost:3306/lcd_rest", user = "root", password = "root", driver = "com.mysql.jdbc.Driver") withSession {
-      val iwc = new IndexWriterConfig(Version.LUCENE_43, analyzer)
-      val writer = new IndexWriter(index, iwc)
-
-      println("Indexing Definition Keywords...")
-      var i = 0
-      Query(DefinitionKeywordsTable) foreach { case (definitionId, keyword) =>
-        val doc = new Document
-        doc.add(new StringField("id", definitionId, Field.Store.YES))
-        doc.add(new TextField("keyword", keyword, Field.Store.YES))
-        writer.addDocument(doc)
-        i+=1
-      }
-      println("Indexed " + i + " keywords.")
-
-      println("Indexing Definition Properties...")
-      i = 0
-      Query(DefinitionProperties) foreach { case (definitionId, property, value) =>
-        val doc = new Document
-        doc.add(new StringField("id", definitionId, Field.Store.YES))
-        doc.add(new TextField(property.toLowerCase, value, Field.Store.YES))
-        writer.addDocument(doc)
-        i+=1
-      }
-      println("Indexed " + i + " properties.")
-
-      writer.close()
-    }
-  }
+  val index = new SimpleFSDirectory(new File("/Users/m091355/Downloads/lucene"))
+//  val index = new RAMDirectory()
+//  def indexAll {
+//    val analyzer = new StandardAnalyzer(Version.LUCENE_43)
+//    Database.forURL("jdbc:mysql://localhost:3306/lcd_rest", user = "root", password = "root", driver = "com.mysql.jdbc.Driver") withSession {
+//      val iwc = new IndexWriterConfig(Version.LUCENE_43, analyzer)
+//      val writer = new IndexWriter(index, iwc)
+//
+//      println("Indexing Definition Keywords...")
+//      var i = 0
+//      Query(DefinitionKeywords) foreach { case (definitionId, keyword) =>
+//        val doc = new Document
+//        doc.add(new StringField("id", definitionId, Field.Store.YES))
+//        doc.add(new TextField("keyword", keyword, Field.Store.YES))
+//        writer.addDocument(doc)
+//        i+=1
+//      }
+//      println("Indexed " + i + " keywords.")
+//
+//      println("Indexing Definition Properties...")
+//      i = 0
+//      Query(DefinitionProperties) foreach { case (definitionId, property, value) =>
+//        val doc = new Document
+//        doc.add(new StringField("id", definitionId, Field.Store.YES))
+//        doc.add(new TextField(property.toLowerCase, value, Field.Store.YES))
+//        writer.addDocument(doc)
+//        i+=1
+//      }
+//      println("Indexed " + i + " properties.")
+//
+//      writer.close()
+//    }
+//  }
 
   def search(field: String, q: String) {
     val lField = field.toLowerCase
